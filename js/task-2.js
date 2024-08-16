354fc88f7cf082bce1d0a1e0af779bfe218efbd0
mirs-me <mir.svetoslavova@gmail.com>
2024-08-16 20:48:06 +0200

    tasks done

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(item){
+        this.#items.push(item);
+    }
+    removeItem(item){
+        const newArray = [];
+        for (let element of this.#items)
+        {if (element != item){
+            newArray.push(element);}
+        }
+        return this.#items = newArray;
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
